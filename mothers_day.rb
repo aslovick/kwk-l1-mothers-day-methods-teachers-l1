47d881743e482952630234ba177ea3aa617f7a9d
Abigail Slovick <abby.slovick@gmail.com>
2018-06-05 21:37:06 +0000

    Done.

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,6 +1,8 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
-
-
+def mothers_day(mother = "Mom")
+  puts "Happy mothers day, #{mother}! I hope you have a great day!"
+end
+mothers_day
+mothers_day("Tracy")
 
 ## Call your method below - use puts to see it in the terminal!
-
